c74afd5518b148452c69c7632718e789452c749c
suyeonsu <agew0423@daum.net>
2023-05-23 16:42:43 +0900

    feat: attraction pagination

--- backend/penetrate_pjt/src/main/java/com/ssafy/group5/controller/AttractionController.java
+++ backend/penetrate_pjt/src/main/java/com/ssafy/group5/controller/AttractionController.java
@@ -1,7 +1,6 @@
 package com.ssafy.group5.controller;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ssafy.group5.dto.Attraction;
 import com.ssafy.group5.model.service.AttractionService;
 
@@ -27,9 +29,10 @@ public class AttractionController {
 	private AttractionService attractionService;
 	
 	@GetMapping
-	public ResponseEntity<List<Attraction>> getAttractionList(@RequestBody Map<String, Object> param) throws SQLException {
+	public ResponseEntity<PageInfo<Attraction>> getAttractionList(@RequestParam int pageNum, @RequestParam int pageSize, @RequestBody Map<String, Object> param) throws SQLException {
 		log.debug("getAttractionList에서 전달받은 파라미터: {}", param);
-		return new ResponseEntity<List<Attraction>>(attractionService.getAttractionList(param), HttpStatus.OK);
+		PageHelper.startPage(pageNum, pageSize);
+		return new ResponseEntity<PageInfo<Attraction>>(PageInfo.of(attractionService.getAttractionList(param)), HttpStatus.OK);
 	}
 	
 	@GetMapping("/detail/{id}")
